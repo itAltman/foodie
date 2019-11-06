955740cf616bfd49f03eb19f55d13db7da2bd99d
puhuang <puhuang.wy@raycloud.com>
2019-11-06 11:38:51 +0800

    feature:集成AOP通过日志监控Service执行时间

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ B2C电商从 0 - 1 再到 100。
 -[x] HikariCP 数据源整合 Mybatis
 -[x] 整合 swagger2 生成接口文档
 -[x] 设置跨域配置实现前后端联调
--[ ] 整合 Log4j 打印日志
--[ ] SpringBoot 配置切面监控 Service 执行时间
+-[x] 整合 Log4j 打印日志
+-[x] SpringBoot 配置切面监控 Service 执行时间
 ### 1. 用户模块
 -[x] 用户注册
 -[x] 用户登录
--- foodie-api/src/main/java/me/atm/foodie/controller/HelloController.java
+++ foodie-api/src/main/java/me/atm/foodie/controller/HelloController.java
@@ -1,22 +1,29 @@
 package me.atm.foodie.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
 /**
- * @RestController 默认返回的数据是 json 格式的
- *
  * @author Altman
+ * RestController 注解默认返回的数据是 json 格式的
  * @date 2019/11/03
  **/
 //@Controller
 @RestController
 @ApiIgnore
 public class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("hello")
     public Object hello() {
+        // 日志级别 ERROR>WARN>INFO>DEBUG
+        log.info("info ==== hello");
+        log.debug("debug ==== hello");
+        log.warn("warn ==== hello");
+        log.error("error ==== hello");
         return "Hello World!!!";
     }
 }
